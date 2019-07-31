5a8b303ddf2778c0eaf1751d3fbfb4ab0df8bdbc
Marcus Spiegel <spiegel@uscreen.de>
2019-07-31 21:01:07 +0200

    WIP: modularize… functional

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const mailer = require('../index')({
-  // load defaults from .env file
+  // load config defaults from .env file
   useDotenv: true,
 
-  // override with inline options
+  // override with inline options if needed
   postalSender: 'domains+noreply@uscreen.net'
 })
 
 console.log(mailer)
+
+console.log(mailer.readTemplate('test'))
